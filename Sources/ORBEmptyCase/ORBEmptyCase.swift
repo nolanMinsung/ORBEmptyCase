b6fc1e02402f2377f4b5c3c1bd8ced6c7e6ea606
Minsung Kim <mskim4048@naver.com>
2025-03-28 15:28:16 +0900

    ORMEmptyCaseStyle 내용 추가

--- Sources/ORBEmptyCase/ORBEmptyCase.swift
+++ Sources/ORBEmptyCase/ORBEmptyCase.swift
@@ -1,2 +1,41 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import UIKit
+
+import SnapKit
+
+
+protocol ORBEmptyCaseStyle: UIView {
+    func showEmptyPlaceholder(view: some ORBEmptyCaseViewType)
+    func removeEmptyPlaceholder()
+}
+
+extension ORBEmptyCaseStyle {
+    
+    func showEmptyPlaceholder(view: some ORBEmptyCaseViewType) {
+        addSubview(view)
+        view.snp.makeConstraints { make in
+            if let scrollView = self as? UIScrollView {
+                make.edges.equalTo(scrollView.frameLayoutGuide)
+            } else {
+                make.edges.equalToSuperview()
+            }
+        }
+        layoutIfNeeded()
+    }
+    
+    func removeEmptyPlaceholder() {
+        subviews.forEach { view in
+            if view is ORBEmptyCaseViewType {
+                view.removeFromSuperview()
+            }
+        }
+    }
+    
+}
+
+protocol ORBEmptyCaseViewType: UIView {
+    var emptyCaseMessage: String { get }
+    var emptyCaseImage: UIImage { get }
+}
