aaf12bcf46da2c9d98a04c6a5d9891c409e64e4f
Minsung Kim <mskim4048@naver.com>
2025-03-28 15:27:44 +0900

    SnapKit 의존성 추가

--- Package.swift
+++ Package.swift
@@ -11,11 +11,17 @@ let package = Package(
             name: "ORBEmptyCase",
             targets: ["ORBEmptyCase"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ORBEmptyCase"),
+            name: "ORBEmptyCase",
+            dependencies: [
+                .product(name: "SnapKit", package: "SnapKit")
+            ]),
         .testTarget(
             name: "ORBEmptyCaseTests",
             dependencies: ["ORBEmptyCase"]
